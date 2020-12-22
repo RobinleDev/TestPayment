edf9ce0f862309c196feb668331e3d223f6cf63a
RobinS <robin.le.dev@protonmail.com>
2020-12-22 18:51:47 +0100

    mise en palce d'un form et test

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log('Username', req.body.username)
+  console.log('Password', req.body.password)
 });
+
 
 module.exports = router;
